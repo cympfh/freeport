7bacf0212c05560ee84345cc38dd12bdba2424e7
cympfh <cympfh@gmail.com>
2019-12-05 18:24:43 +0900

    versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 
 setup(name='freeport',
-      version='1.0',
+      version='1.0.0',
       description='Scan Free Port',
       author='cympfh',
       author_email='cympfh@gmail.com',
